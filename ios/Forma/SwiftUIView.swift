73d34816383cd6245691b41faf522c91008efc9a
aneeshpatne <aneeshpatne@gmail.com>
2026-06-19 13:43:28 +0530

    Add main view with header and tab navigation

--- ios/Forma/SwiftUIView.swift
+++ ios/Forma/SwiftUIView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
+        VStack {
+            FormaHeader()
 
+            TabView {
+                MetricsView().tabItem {
+                    Image(systemName: "chart.xyaxis.line")
+                    Text("Metrics")
+                }
+                
+                WorkoutsView().tabItem {
+                    Image(systemName: "figure.strengthtraining.traditional")
+                    Text("Workouts")
+                }
+
+                RecordView().tabItem {
+                    Image(systemName: "record.circle")
+                    Text("Record")
+                }
+
+                VitalsView().tabItem {
+                    Image(systemName: "heart.text.square")
+                    Text("Vitals")
+                }
+            }
+        }
     }
 }
 
