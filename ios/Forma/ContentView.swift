ce0cf9dd8a346370db578a99368df8dc1b1cde1b
aneeshpatne <aneeshpatne@gmail.com>
2026-06-19 11:24:43 +0530

    Initial Commit

---
+++ ios/Forma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Forma
+//
+//  Created by Aneesh Patne on 19/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/Forma/FormaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormaApp.swift
+//  Forma
+//
+//  Created by Aneesh Patne on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FormaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
