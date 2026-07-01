4dad95ef195f88b57274083dd1343ca903a7536a
aneeshpatne <aneeshpatne@gmail.com>
2026-07-01 12:07:37 +0530

    Add JSONValue helper for flexible API decoding

---
+++ ios/Forma/Networking/JSONValue.swift
@@ -0,0 +1,35 @@
+//
+//  JSONValue.swift
+//  Forma
+//
+//  Created by Codex on 30/06/26.
+//
+
+import Foundation
+
+enum JSONValue: Decodable, Equatable {
+    case string(String)
+    case number(Double)
+    case bool(Bool)
+    case object([String: JSONValue])
+    case array([JSONValue])
+    case null
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if container.decodeNil() {
+            self = .null
+        } else if let value = try? container.decode(Bool.self) {
+            self = .bool(value)
+        } else if let value = try? container.decode(Double.self) {
+            self = .number(value)
+        } else if let value = try? container.decode(String.self) {
+            self = .string(value)
+        } else if let value = try? container.decode([JSONValue].self) {
+            self = .array(value)
+        } else {
+            self = .object(try container.decode([String: JSONValue].self))
+        }
+    }
+}
